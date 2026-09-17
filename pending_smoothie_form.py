d2b37f49bb9b5b3a482ddbad7200c580c31d6b6d
PonGopika <168147459+PonGopika@users.noreply.github.com>
2026-09-17 21:00:06 +0530

    pending_order_smoothie

--- pending_smoothie_form.py
+++ pending_smoothie_form.py
@@ -1,10 +1,10 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col, when_matched
 
 # Get the current credentials
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.orders") \
     .filter(col("ORDER_FILLED") == 0) \
