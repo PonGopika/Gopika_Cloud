b5f4eda32b0ec017d696660f761333b2eb120cec
PonGopika <168147459+PonGopika@users.noreply.github.com>
2026-09-17 21:33:57 +0530

    e

--- Gopika_custom_smoothie.py
+++ Gopika_custom_smoothie.py
@@ -35,9 +35,8 @@ if ingredients:
     ingredients_string += fruit_chosen + " "
 
     smoothiefroot_response = requests.get(
-      "https://my.smoothiefroot.com/api/fruit/" + fruit_chosen.lower()
-    )
-
+    "https://my.smoothiefroot.com/api/fruit/" + fruit_chosen
+)
     sf_df = st.dataframe(
       data=smoothiefroot_response.json(),
       use_container_width=True
