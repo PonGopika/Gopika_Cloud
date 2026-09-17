b17f2d1dce0b02b3f9c0663ada3e1414f5eb3ffe
PonGopika <168147459+PonGopika@users.noreply.github.com>
2026-09-17 21:36:49 +0530

    edited back

--- Gopika_custom_smoothie.py
+++ Gopika_custom_smoothie.py
@@ -34,9 +34,7 @@ if ingredients:
   for fruit_chosen in ingredients:
     ingredients_string += fruit_chosen + " "
 
-    smoothiefroot_response = requests.get(
-    "https://my.smoothiefroot.com/api/fruit/" + fruit_chosen
-)
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
     sf_df = st.dataframe(
       data=smoothiefroot_response.json(),
       use_container_width=True
