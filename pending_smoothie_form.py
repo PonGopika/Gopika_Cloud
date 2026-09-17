ec1f913c3c6b64623a4572a673347c2970bcbe7d
PonGopika <168147459+PonGopika@users.noreply.github.com>
2026-09-17 20:55:07 +0530

    pending smoothie orders

---
+++ pending_smoothie_form.py
@@ -0,0 +1,35 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col, when_matched
+
+# Get the current credentials
+session = get_active_session()
+
+my_dataframe = session.table("smoothies.public.orders") \
+    .filter(col("ORDER_FILLED") == 0) \
+    .collect()
+
+if len(my_dataframe) > 0:
+
+    editable_df = st.data_editor(my_dataframe)
+
+    submit = st.button("Submit Changes")
+
+    if submit:
+        og_dataset = session.table("smoothies.public.orders")
+        edited_dataset = session.create_dataframe(editable_df)
+
+        merge_result = og_dataset.merge(
+            edited_dataset,
+            (og_dataset["ORDER_UID"] == edited_dataset["ORDER_UID"]),
+            [when_matched().update(
+                {"ORDER_FILLED": edited_dataset["ORDER_FILLED"]}
+            )]
+        )
+
+        if merge_result:
+            st.success("Changes saved!", icon="👍")
+
+else:
+    st.write("No pending orders.")
